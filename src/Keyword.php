f4bfe3a90af6fa8bc4b9e2be3c14f6d4c53a32f4
Kristof Coomans <kristof@coomans.eu>
2014-12-14 10:17:33 +0100

    Moved classes over from UDB3 project

---
+++ src/Keyword.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\Entry;
+
+class Keyword implements \JsonSerializable
+{
+    protected $value;
+
+    public function __construct($value)
+    {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('Keyword should be a string');
+        }
+
+        $value = trim($value);
+        if ('' === $value) {
+            throw new \InvalidArgumentException('Keyword should consist of at least one character');
+        }
+
+        if (false !== strpos($value, ';')) {
+            throw new \InvalidArgumentException('Keyword should not contain semicolons');
+        }
+        $this->value = $value;
+    }
+
+    public function __toString()
+    {
+        return $this->value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize()
+    {
+        return $this->value;
+    }
+}
